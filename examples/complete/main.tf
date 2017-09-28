1209e0ea7125cf698464f14eb3975ca2111b8303
Felipe Martin <me@fmartingr.com>
2017-09-28 11:27:42 +0200

    Set example storage_encrypted value to false

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -30,7 +30,7 @@ module "db" {
   engine_version    = "5.7.11"
   instance_class    = "db.t2.large"
   allocated_storage = 5
-  storage_encrypted = true
+  storage_encrypted = false
   # kms_key_id        = "arm:aws:kms:<region>:<accound id>:key/<kms key id>"
 
   name     = "demodb"
