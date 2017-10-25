491933a1e5ec3c57ecfc46a391765c8f0c20c7af
Quentin Rousseau <quentin@instacart.com>
2017-10-24 18:23:31 -0700

    Fixed replica support
    db_subnet_group_name must *NOT* be specified when creating aws_rds_cluster_instance replica resource

--- main.tf
+++ main.tf
@@ -3,6 +3,7 @@
 ##################
 module "db_subnet_group" {
   source = "./modules/db_subnet_group"
+  count  = "${var.replicate_source_db == "" ? 1 : 0}"
 
   identifier  = "${var.identifier}"
   name_prefix = "${var.identifier}-"
@@ -54,7 +55,7 @@ module "db_instance" {
   snapshot_identifier = "${var.snapshot_identifier}"
 
   vpc_security_group_ids = ["${var.vpc_security_group_ids}"]
-  db_subnet_group_name   = "${module.db_subnet_group.this_db_subnet_group_id}"
+  db_subnet_group_name   = "${var.replicate_source_db == "" ? module.db_subnet_group.this_db_subnet_group_id : ""}"
   parameter_group_name   = "${module.db_parameter_group.this_db_parameter_group_id}"
 
   multi_az            = "${var.multi_az}"
