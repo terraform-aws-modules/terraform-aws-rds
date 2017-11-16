7d1ad3627e860afa8d99d9e732af289f5a1983dc
Martin Atkins <mart@degeneration.co.uk>
2017-11-16 03:28:58 -0800

    db_subnet_group: fix failing output expressions (#22)
    Accessing aws_db_subnet_group.this when its count = 0 is an error, so we
    need to instead access it via splat syntax and then apply a default value
    for when the resulting list is empty.

--- modules/db_subnet_group/outputs.tf
+++ modules/db_subnet_group/outputs.tf
@@ -1,10 +1,10 @@
 # DB subnet group
 output "this_db_subnet_group_id" {
   description = "The db subnet group name"
-  value       = "${var.count == 0 ? "" : aws_db_subnet_group.this.id}"
+  value       = "${element(concat(aws_db_subnet_group.this.*.id, list("")), 0)}"
 }
 
 output "this_db_subnet_group_arn" {
   description = "The ARN of the db subnet group"
-  value       = "${aws_db_subnet_group.this.arn}"
+  value       = "${element(concat(aws_db_subnet_group.this.*.arn, list("")), 0)}"
 }
