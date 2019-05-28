346f1496146cbd90248b8545354d6de7f2236b16
Pablo Serrano <pablo@pabloserrano.cc>
2019-05-28 15:18:21 +0200

    fix dbinstance main 2

--- modules/db_instance/variables.tf
+++ modules/db_instance/variables.tf
@@ -81,7 +81,7 @@ variable "final_snapshot_identifier" {
 }
 
 variable "vpc_security_group_ids" {
-  type        = "list(string)"
+  type        = "list"
   description = "List of VPC security groups to associate"
   default     = []
 }
