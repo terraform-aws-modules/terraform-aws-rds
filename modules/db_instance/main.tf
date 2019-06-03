56c23bbf06b1eff8eb0543b537b5d25dbefa4f34
Pablo Serrano <pablo@pabloserrano.cc>
2019-06-03 09:21:45 +0100

    Transform variable list of list

--- modules/db_instance/main.tf
+++ modules/db_instance/main.tf
@@ -46,7 +46,7 @@ resource "aws_db_instance" "this" {
 
   snapshot_identifier = var.snapshot_identifier
 
-  vpc_security_group_ids = var.vpc_security_group_ids
+  vpc_security_group_ids = [var.vpc_security_group_ids]
   db_subnet_group_name   = var.db_subnet_group_name
   parameter_group_name   = var.parameter_group_name
   option_group_name      = var.option_group_name
