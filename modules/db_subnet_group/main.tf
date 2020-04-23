3bfb89b7f510b154b06cde797b7f179ccfa6c5c2
marcportabellaclotet-mt <56038098+marcportabellaclotet-mt@users.noreply.github.com>
2020-04-23 17:13:04 +0200

    fix subnet gtoup naming

--- modules/db_subnet_group/main.tf
+++ modules/db_subnet_group/main.tf
@@ -1,30 +1,17 @@
 resource "aws_db_subnet_group" "this_no_prefix" {
   count = var.create && false == var.use_name_prefix ? 1 : 0
-
+  name = var.identifier
   description = "Database subnet group for ${var.identifier}"
   subnet_ids  = var.subnet_ids
-
-  tags = merge(
-    var.tags,
-    {
-      "Name" = format("%s", var.identifier)
-    },
-  )
+  tags = var.tags
 }
-
 
 resource "aws_db_subnet_group" "this" {
   count = var.create && var.use_name_prefix ? 1 : 0
-
+  name = var.identifier
   name_prefix = var.name_prefix
   description = "Database subnet group for ${var.identifier}"
   subnet_ids  = var.subnet_ids
-
-  tags = merge(
-    var.tags,
-    {
-      "Name" = format("%s", var.identifier)
-    },
-  )
+  tags = var.tags
 }
 
