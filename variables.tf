56507579008b4e145083a58f3e5d462b26b86338
Ezbon Jacob <ezjacob@gmail.com>
2021-03-10 10:39:30 -0500

    fix: default password so create_password does not require password var

--- variables.tf
+++ variables.tf
@@ -97,6 +97,7 @@ variable "username" {
 variable "password" {
   description = "Password for the master DB user. Note that this may show up in logs, and it will be stored in the state file"
   type        = string
+  default     = ""
 }
 
 variable "port" {
