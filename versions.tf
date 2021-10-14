1d440b12df6779cf41a4d7fdb4d72bee87bac5c4
Mukesh Sharma <mukeshsharma24@gmail.com>
2021-10-15 09:14:38 +1300

    bump tf aws provider version

--- versions.tf
+++ versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 2.49"
+      version = ">= 3.62"
     }
     random = {
       source  = "hashicorp/random"
