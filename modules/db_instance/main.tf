bccd6df378284f1af3547f9c4ef5aeaeeba4a887
Anton Babenko <anton@antonbabenko.com>
2022-01-12 08:29:06 +0100

    Update modules/db_instance/main.tf

--- modules/db_instance/main.tf
+++ modules/db_instance/main.tf
@@ -115,7 +115,6 @@ resource "aws_db_instance" "this" {
     delete = lookup(var.timeouts, "delete", null)
     update = lookup(var.timeouts, "update", null)
   }
-  
   lifecycle {
     ignore_changes = [
       latest_restorable_time
