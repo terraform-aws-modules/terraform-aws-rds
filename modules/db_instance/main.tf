77902c21414f24026d720e72068e589b3855fab8
Karol Wójcik <karol.wojcik@tuta.io>
2022-01-12 08:33:20 +0100

    feat: Added `latest_restorable_time` to ignore_changes (#356)
    Co-authored-by: Anton Babenko <anton@antonbabenko.com>

--- modules/db_instance/main.tf
+++ modules/db_instance/main.tf
@@ -115,6 +115,12 @@ resource "aws_db_instance" "this" {
     delete = lookup(var.timeouts, "delete", null)
     update = lookup(var.timeouts, "update", null)
   }
+
+  lifecycle {
+    ignore_changes = [
+      latest_restorable_time
+    ]
+  }
 }
 
 resource "aws_db_instance" "this_mssql" {
