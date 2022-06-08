3a22b5b20d178e7540b12b717fe2d6d670234097
Tamir Friedman <74854258+TamirFriedman-RecoLabs@users.noreply.github.com>
2022-06-08 12:25:09 +0300

    fix warning in module
    $ terraform validate
    ╷
    │ Warning: Redundant ignore_changes element
    │ 
    │   on .terraform/modules/postgres/modules/db_instance/main.tf line 26, in resource "aws_db_instance" "this":
    │   26: resource "aws_db_instance" "this" {
    │ 
    │ Adding an attribute name to ignore_changes tells Terraform to ignore future changes to the argument in configuration after the object has been created, retaining the
    │ value originally configured.
    │ 
    │ The attribute latest_restorable_time is decided by the provider alone and therefore there can be no configured value to compare with. Including this attribute in
    │ ignore_changes has no effect. Remove the attribute from ignore_changes to quiet this warning.

--- modules/db_instance/main.tf
+++ modules/db_instance/main.tf
@@ -116,12 +116,6 @@ resource "aws_db_instance" "this" {
     delete = lookup(var.timeouts, "delete", null)
     update = lookup(var.timeouts, "update", null)
   }
-
-  lifecycle {
-    ignore_changes = [
-      latest_restorable_time
-    ]
-  }
 }
 
 ################################################################################
