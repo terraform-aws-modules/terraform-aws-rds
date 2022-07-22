92df7a39b71f57b1ad830680b5c864983c05b439
Ravindra Singh <51900358+ravindrasinghh@users.noreply.github.com>
2022-07-22 22:32:52 +0530

    Update variables.tf

--- modules/db_instance/variables.tf
+++ modules/db_instance/variables.tf
@@ -105,12 +105,6 @@ variable "username" {
   default     = null
 }
 
-variable "password" {
-  description = "Password for the master DB user. Note that this may show up in logs, and it will be stored in the state file"
-  type        = string
-  default     = null
-}
-
 variable "port" {
   description = "The port on which the DB accepts connections"
   type        = string
