6854f4d7720230d2badeee2c0c69f8b014447480
magreenbaum <magreenbaum>
2023-02-12 10:34:39 -0500

    update cloudwatch log group creation conditional

--- modules/db_instance/main.tf
+++ modules/db_instance/main.tf
@@ -147,7 +147,7 @@ resource "aws_db_instance" "this" {
 ################################################################################
 
 resource "aws_cloudwatch_log_group" "this" {
-  for_each = toset([for log in var.enabled_cloudwatch_logs_exports : log if var.create && var.create_cloudwatch_log_group])
+  for_each = toset([for log in var.enabled_cloudwatch_logs_exports : log if var.create && var.create_cloudwatch_log_group && !var.use_identifier_prefix])
 
   name              = "/aws/rds/instance/${var.identifier}/${each.value}"
   retention_in_days = var.cloudwatch_log_group_retention_in_days
