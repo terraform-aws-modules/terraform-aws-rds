927b714cdad3c8b1f8e28c3b0fb35f962916630f
Melissa Greenbaum <69476188+magreenbaum@users.noreply.github.com>
2023-02-12 16:07:55 -0500

    fix: Update cloudwatch log group creation conditional (#481)
    Co-authored-by: magreenbaum <magreenbaum>

--- modules/db_instance/main.tf
+++ modules/db_instance/main.tf
@@ -146,8 +146,9 @@ resource "aws_db_instance" "this" {
 # CloudWatch Log Group
 ################################################################################
 
+# Log groups will not be created if using an identifier prefix
 resource "aws_cloudwatch_log_group" "this" {
-  for_each = toset([for log in var.enabled_cloudwatch_logs_exports : log if var.create && var.create_cloudwatch_log_group])
+  for_each = toset([for log in var.enabled_cloudwatch_logs_exports : log if var.create && var.create_cloudwatch_log_group && !var.use_identifier_prefix])
 
   name              = "/aws/rds/instance/${var.identifier}/${each.value}"
   retention_in_days = var.cloudwatch_log_group_retention_in_days
